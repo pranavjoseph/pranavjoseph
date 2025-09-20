59eba808ef7a1b0285568b51a1fd0b655e19c80d
Joseph Pranav <pranavjoseph69@gmail.com>
2025-09-20 12:55:22 +0100

    update base

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
+const BASE = process.env.VITE_BASE_URL !== undefined ? process.env.VITE_BASE_URL : process.env.NODE_ENV === "production" ? "/pranav_dev/" : "/";
+
+console.log("🚀 Using base:", BASE);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/pranav_dev/' : '/',
+  base: BASE,
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
