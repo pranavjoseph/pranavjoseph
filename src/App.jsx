026878966895cc45d00cd4559d16892959565e24
Joseph Pranav <pranavjoseph69@gmail.com>
2025-09-21 16:16:27 +0100

    remove redirct

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router'
-import { useEffect } from 'react';
-import './App.css'
-import HomePage from './pages/Home'
-import PageNotFound from './pages/404';
-import { BASE_URL } from './utils/constants';
-import ContactPage from './pages/Contact';
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router";
+import { useEffect } from "react";
+import "./App.css";
+import HomePage from "./pages/Home";
+import PageNotFound from "./pages/404";
+import { BASE_URL } from "./utils/constants";
+import ContactPage from "./pages/Contact";
+import NodeJsDevPage from "./pages/ServiceLandingPage/NodeJsDev";
+import FullStackDev from "./pages/ServiceLandingPage/FullStackDev";
 
+// function RedirectHandler() {
+//   const navigate = useNavigate();
 
-function RedirectHandler() {
-  const navigate = useNavigate();
+//   useEffect(() => {
+//     const params = new URLSearchParams(window.location.search);
+//     const path = params.get("p");
+//     if (path) {
+//       navigate(path, { replace: true });
+//     }
+//   }, [navigate]);
 
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const path = params.get("p");
-    if (path) {
-      navigate(path, { replace: true });
-    }
-  }, [navigate]);
-
-  return null;
-}
+//   return null;
+// }
 
 function App() {
-
   return (
     <BrowserRouter basename={BASE_URL}>
-      <RedirectHandler />
+      {/* <RedirectHandler /> */}
       <Routes>
-        <Route index path='/' element={<HomePage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='*' element={<PageNotFound />} />
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+        <Route path="/nodejs-developer" element={<NodeJsDevPage />} />
+        <Route path="/fullstack-developer" element={<FullStackDev />} />
+
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
-export default App
+export default App;
