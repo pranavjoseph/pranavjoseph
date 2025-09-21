d1f989aa9231ca0f19ba697dd8a97c38c688663e
Joseph Pranav <pranavjoseph69@gmail.com>
2025-09-21 15:37:36 +0100

    form update

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -25,7 +25,8 @@ function ContactPage() {
           </Link>
         </p>
 
-        <form action="https://formspree.io/f/mkgvjlqk" method="POST" className="w-full max-w-md bg-gray-100 dark:bg-gray-800 p-6 rounded-2xl shadow-lg space-y-4">
+        <form netlify method="POST" className="w-full max-w-md bg-gray-100 dark:bg-gray-800 p-6 rounded-2xl shadow-lg space-y-4">
+          <input type="hidden" name="form-name" value="contact" />
           <div>
             <label className="block text-gray-700 dark:text-gray-300 mb-2">Name</label>
             <input type="text" name="name" required className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 outline-none" />
